7aefed6553581e265aacdf90331a6459b52e61a3
alberthaciverdiyev1 <alberthaciverdiyev55@gmail.com>
2025-01-03 23:24:09 +0400

    remove unused methods from ViewController

--- src/Controllers/ViewController.js
+++ src/Controllers/ViewController.js
@@ -48,55 +48,7 @@ const ViewController = {
             js: 'ContactUs.js'
         };
         res.render('Main', view);
-    },
-    addJob: async (req, res) => {
-        const view = {
-            title: 'Yeni vakansiya',
-            body: "Jobs/Add.ejs",
-            js: 'NewJob.js'
-        };
-        res.render('Main', view);
-    },
-    adminIndex: async (req, res) => {
-        const view = {
-            title: 'Admin Panel',
-            body: "Home/Index.ejs",
-            js: "Index.js"
-        };
-        res.render('Admin/Main', view);
-    },
-
-    adminCategoryView: async (req, res) => {
-        const view = {
-            title: 'Categories - Admin Panel',
-            body: "Category/Index.ejs",
-            js: "Category.js"
-        };
-        res.render('Admin/Main', view);
-    },
-
-
-    // education: (req, res) => {
-    //     const educationData = Enums.Education;
-
-    //     res.status(200).json(educationData);
-    // },
-    // experience: (req, res) => {
-    //     const experience = Enums.Experience;
-    //     res.status(200).json(experience);
-    // },
-    // sendMail: async (req, res) => {
-    //     const response = await sendEmail(req.body.data);
-    //     res.status(200).json({ status: response.status, message: response.message });
-
-    // },
-    // statistics: async (req, res) => {
-    //     const company = await CompanyService.count()
-    //     const vacancy = await JobDataService.count()
-    //     const visitor = await VisitorService.count()
-    //     res.status(200).json({ status: 200, message:"",data:{company,vacancy,visitor} });
-    
-    // }
+    }
 };
 
 export default ViewController;
